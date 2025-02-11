906e20fc574db9f8ac4e4a024d845eabab591a8b
CodeshShubh <codeshshubh@gmail.com>
2025-02-11 19:06:17 +0530

    complete

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,21 @@
+import React, { useState } from 'react';
 
+const Selection = ({ index, selectedColor }) => {
+  const [boxColor, setBoxColor] = useState({ background: "" });
+
+  const handleBoxClick = () => {
+    setBoxColor(selectedColor);
+  };
+
+  return (
+    <div
+      className="fix-box"
+      style={{ ...boxColor, backgroundColor: boxColor.background || 'rgb(255, 153, 51)' }}
+      onClick={handleBoxClick}
+    >
+      Selection {index + 1}
+    </div>
+  );
+};
+
+export default Selection;
